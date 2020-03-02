ffd510d7ef8e4a1ed92aa298503712c27431bb1e
Jesse Gould <jesse.gould@codeimmersives.com>
2020-03-02 09:59:40 -0500

    for turn in

--- index.js
+++ index.js
@@ -1,66 +1,60 @@
 const express = require('express');
 const app = express();
 const logger = require('morgan');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
+const fetch = require('node-fetch');
 const path = require('path');
-let MongoStore = require('connect-mongo')(session);
-const random = require ('./models/random')
-// const movies = require ('./models/movies')
-
+const mongoose = require('mongoose');
 
 require('dotenv').config();
-const port=process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGODB_URI,{
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
 })
-.then(()=> {
-    console.log('Mongodb connected');
-})
-.catch(err => console.log(`Mongodb error: ${err}`));
-
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+    .then(() => console.log('MongoDB Connected'))
+    .catch((err) => console.log(`Mongo Error: ${err}`));
 
 app.use(logger('dev'));
-app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
-app.use(session({
-    resave: true,
-    saveUninitialized: true,
-    secret: process.env.SESSION_SECRET,
-    store: new MongoStore({
-        url: process.env.MONGODB_URI,
-        mongooseConnection : mongoose.connection,
-        autoReconnect : true,
-    }),
-    cookie:{
-        secure: false,
-        maxAge: 600000
-    }
-})
-);
+app.use(express.urlencoded({ extended: true}));
 
+app.set('view engine', 'ejs');
 
-app.get('/random/', (req, res)=>{
-    res.render('main/random', {random});
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/randomusers', (req, res) => {
+    const url = 'https://randomuser.me/api/?results=20';
+
+    fetch(url)
+        .then((res) => res.json())
+        .then((users) => {
+            const sortedUsers = users.results.sort((a, b) => (a.name.last > b.name.last) ? 1 : ((b.name.last > a.name.last) ? -1 : 0))
+            res.render('main/random', { sortedUsers });
+        })
+        .catch((err) => console.log(err));
 });
 
-// app.get('/movies/', (req, res)=>{
-//     res.render('main/random', {movies});
-// });
+app.get('/movies', (req, res) => {
+    const url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=';
+    const urlEnd = '&language=en-US&page=1';
+    const apiKey = process.env.API_KEY
+    const img = 'https://image.tmdb.org/t/p/w500';
 
+    fetch(url+apiKey+urlEnd)
+        .then((res) => res.json())
+        .then((movies) => {
+            res.render('main/movies', { movies, img })
+        })
+        .catch((err) => console.log(err))
+});
 
-
-
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
+
+
 
 
